7fb9c60b4b135ebd0b0a3fcf349b38c6433548b6
Chioma750 <ugwuchioma778@gmail.com>
2026-03-31 02:49:31 +0100

    day3

--- string-transformer/main.go
+++ string-transformer/main.go
@@ -25,16 +25,75 @@ func Capitalize(name string) string {
 	return strings.Title(n)
 }
 
+func Title(name string) string {
+	word := map[string]bool{
+		"a":   true,
+		"an":  true,
+		"the": true,
+		"but": true,
+		"or":  true,
+		"for": true,
+		"nor": true,
+		"on":  true,
+		"at":  true,
+		"to":  true,
+		"by":  true,
+		"in":  true,
+		"of":  true,
+		"up":  true,
+		"as":  true,
+		"is":  true,
+		"it":  true,
+	}
+	text := strings.Fields(name)
+	for i, w := range text {
+		if i == 0 || !word[w] {
+			text[i] = strings.ToUpper(string(w[0])) + w[1:]
+		}
+	}
+	return strings.Join(text, " ")
+}
+
+func snake(name string) string {
+	w := strings.ToLower(name)
+	return strings.ReplaceAll(w, " ", "_")
+}
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Text: ")
-	name, _ := reader.ReadString('\n')
-	fmt.Print("Output 1: ")
-	fmt.Println(UpperCase(name))
+	for {
+		var input string
+		fmt.Print("Enter a word : ")
+		fmt.Scanln(&input)
 
-	fmt.Print("Output 2: ")
-	fmt.Println(LowerCase(name))
+		var operator string
+		fmt.Print("Choose input transformations (UpperCase, LowerCase, Capitalize, Title, snake, exit): ")
+		fmt.Scanln(&operator)
 
-	fmt.Print("Output 3: ")
-	fmt.Println(Capitalize(name))
+		if operator == "exit" {
+			fmt.Println("exiting...")
+			return
+		}
+		if operator != "UpperCase" && operator != "LowerCase" && operator != "Capitalize" && operator != "Title" && operator != "snake" && operator != "exit" {
+			fmt.Println("Unrecognised")
+			continue
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+		name, _ := reader.ReadString('\n')
+
+		//fmt.Print("Output 1: ")
+		fmt.Println(UpperCase(name))
+
+		//fmt.Print("Output 2: ")
+		fmt.Println(LowerCase(name))
+
+		//fmt.Print("Output 3: ")
+		fmt.Println(Capitalize(name))
+
+		//fmt.Print("Output 4: ")
+		fmt.Println(Title(name))
+
+		//fmt.Print("Output 5: ")
+		fmt.Println(snake(name))
+	}
 }
