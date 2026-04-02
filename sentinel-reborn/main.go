59272ff977efb80bf5ee9645619e17f69effe5f5
Cugwu <ugwuchioma778@gmail.com>
2026-04-02 15:23:22 +0100

    my files

---
+++ sentinel-reborn/main.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func readFile(filename string) string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return string(data)
+}
+
+func writeFile(filename string, content string) {
+	err := os.WriteFile(filename, []byte(content+"\n"), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+	}
+}
+
+func main() {
+
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . input.txt output.txt")
+	}
+
+	input1 := os.Args[1]
+	input2 := os.Args[2]
+
+	content := readFile(input1)
+	content = applyTransformation(content)
+	writeFile(input2, content)
+}
+
+func multiPunctuation(text string) string {
+	replacements := map[string]string{
+		" ...": "...",
+		" !!":  "!!",
+		" ??":  "??",
+		" !?":  "!?",
+		" ?!":  "?!",
+	}
+
+	for old, newVal := range replacements {
+		text = strings.ReplaceAll(text, old, newVal)
+	}
+
+	return text
+}
+
+// DONE
+
+func fixPunctuation(text string) string {
+	text = multiPunctuation(text)
+
+	punctuations := []string{".", ",", "!", "?", ":", ";"}
+
+	for _, p := range punctuations {
+		text = strings.ReplaceAll(text, " "+p, p+" ")
+		text = strings.ReplaceAll(text, p+" ", p+" ")
+	}
+
+	text = strings.ReplaceAll(text, "' ", "'")
+	text = strings.ReplaceAll(text, " '", "'")
+
+	return strings.Join(strings.Fields(text), " ")
+}
+
+// DONE
+func fixArticle(word string) string {
+	words := strings.Fields(word)
+	vowels := "aeiouhAEIOUH"
+	for i := 0; i < len(words)-1; i++ {
+		if (words[i] == "a" || words[i] == "A") && strings.ContainsRune(vowels, rune(words[i+1][0])) {
+			if words[i] == "A" {
+				words[i] = "An"
+			} else {
+				words[i] = "an"
+			}
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+// DONE
+func converter(input string) string {
+	words := strings.Fields(input)
+
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(hex)" {
+			if n, err := strconv.ParseInt(words[i-1], 16, 64); err == nil {
+				words[i-1] = strconv.FormatInt(n, 10)
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+
+		} else if words[i] == "(bin)" {
+			if n, err := strconv.ParseInt(words[i-1], 2, 64); err == nil {
+				words[i-1] = strconv.FormatInt(n, 10)
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func capitalizeWord(word string) string {
+	runes := []rune(strings.ToLower(word))
+	if len(runes) > 0 {
+		runes[0] = unicode.ToUpper(runes[0])
+	}
+	return string(runes)
+}
+
+func toLowerCase(text string) string {
+	words := strings.Fields(text)
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(low)" {
+			if i-1 >= 0 {
+				words[i-1] = strings.ToLower(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
+
+		if words[i] == "(low," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			n, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - n
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = strings.ToLower(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func toUpperCase(text string) string {
+	words := strings.Fields(text)
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(up)" {
+			if i-1 >= 0 {
+				words[i-1] = strings.ToUpper(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
+
+		if words[i] == "(up," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			n, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - n
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = strings.ToUpper(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func toTitleCase(text string) string {
+	words := strings.Fields(text)
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == "(cap)" {
+			if i-1 >= 0 {
+				words[i-1] = capitalizeWord(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i--
+			continue
+		}
+
+		if words[i] == "(cap," && i+1 < len(words) {
+			countStr := strings.TrimSuffix(words[i+1], ")")
+			num, err := strconv.Atoi(countStr)
+			if err == nil {
+				start := i - num
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < i; j++ {
+					words[j] = capitalizeWord(words[j])
+				}
+			}
+			words = append(words[:i], words[i+2:]...)
+			i--
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func applyTransformation(s string) string {
+	s = converter(s)
+	s = fixArticle(s)
+	s = fixPunctuation(s)
+	s = toUpperCase(s)
+	s = toLowerCase(s)
+	s = toTitleCase(s)
+
+	return s
+}
--- string-transformer/README.md
+++ string-transformer/README.md
@@ -1,1 +1,10 @@
-## Transmission From HQ
+## String Transformer
+This project is designed in a way that the string transformer takes in corrupted inputs and produce clean correctly and formatted output.
+
+## Running the Application (Usage)
+To run the project you need to type "go run main.go" on your terminal.
+Choose a word or a sentence of your choice you want to transform, choose any of the input transformations listed to you, then hit the enter botton.
+You will get a correctly formatted output.
+
+## Note That
+If you select any operator that is not from the listed given to you, you will not get a correctly formatted output, rather it will print Unrecognised.
